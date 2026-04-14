13bff2396844a4ec7e7292e50611973067c9bfde
Chris Brenton <hiring@rubyridge.org>
2026-04-14 15:57:57 -0400

    Add progress bar

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use image::{Rgb, RgbImage};
+use kdam::{BarExt, tqdm};
 use std::fs;
 
 const IMAGE_W: u32 = 256;
@@ -9,12 +10,17 @@ fn main() {
 
     let mut img = RgbImage::new(IMAGE_W, IMAGE_H);
 
+    let mut bar = tqdm!(total = IMAGE_W as usize * IMAGE_H as usize);
+
     for (x, y, pixel) in img.enumerate_pixels_mut() {
         *pixel = Rgb([x as u8, y as u8, 0]);
+        bar.update(1).unwrap();
     }
 
+    // create output dir
     fs::create_dir("./output");
-    img.save("output/test.png");
 
+    // write image
+    img.save("output/test.png");
     println!("Wrote file to {filename}");
 }
