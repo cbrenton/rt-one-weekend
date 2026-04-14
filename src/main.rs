e810e3d6e372e71e3c6180e6a06194dde767394b
Chris Brenton <hiring@rubyridge.org>
2026-04-14 17:54:03 -0400

    Add dummy ray class. Calculate image_h from image_h and aspect_ratio

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
+use glam::Vec3;
 use kdam::BarExt;
 
-const IMAGE_W: usize = 256;
-const IMAGE_H: usize = 256;
+const IMAGE_W: f32 = 800.0;
+const ASPECT_RATIO: f32 = 16.0 / 9.0;
 
 mod canvas;
 mod color;
+mod ray;
 
 fn main() {
-    let mut img = canvas::Canvas::new(IMAGE_W, IMAGE_H);
+    let image_h = (IMAGE_W / ASPECT_RATIO) as usize;
+    let mut img = canvas::Canvas::new(IMAGE_W as usize, image_h);
 
     let mut bar = img.progress_bar();
+    let r = ray::Ray::new(Vec3::new(0.0, 0.0, 0.0), Vec3::new(0.0, 0.0, 0.0));
 
-    for x in 0..IMAGE_W {
-        for y in 0..IMAGE_H {
-            let pixel_color =
-                color::Color::new(x as f32 / IMAGE_W as f32, y as f32 / IMAGE_H as f32, 0.0);
+    for x in 0..img.width {
+        for y in 0..img.height {
+            let pixel_color = color::Color::new(x as f32 / IMAGE_W, y as f32 / image_h as f32, 0.0);
             img[(x, y)] = pixel_color;
             bar.update(1).unwrap();
         }
