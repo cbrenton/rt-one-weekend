e4a9fb0bd181671676cdd3e810526d455dff3de4
Chris Brenton <hiring@rubyridge.org>
2026-04-22 13:52:24 -0400

    Add random number generator helper functions

--- src/util.rs
+++ src/util.rs
@@ -8,6 +8,20 @@ pub use color::Color;
 pub use interval::Interval;
 pub use ray::Ray;
 
+use rand::prelude::*;
+use std::ops::Range;
+
 // NOTE: for infinity, use f64::INFINITY
 // NOTE: for pi, use f64::consts::PI
 // NOTE: for degrees to radians, use f64::to_radians()
+
+// NOTE: rust inlines small functions automatically. I assume this counts
+/// Returns a double between 0.0 and 1.0.
+pub fn random_double() -> f64 {
+    rand::rng().random_range(0.0..1.0)
+}
+
+/// Returns a double in the given range (exclusive).
+pub fn random_double_range(range: Range<f64>) -> f64 {
+    rand::rng().random_range(range)
+}
--- src/util/color.rs
+++ src/util/color.rs
@@ -1,4 +1,6 @@
 use glam::DVec3;
 
-// a Color is a DVec3 where each component is a float of range [0.0 -> 1.0]
+// a Color is a DVec3 where each component is a double of range [0.0 -> 1.0]
+// TODO: maybe someday convert this to a newtype (but I think I'd have to reimplement all the
+// traits)
 pub type Color = DVec3;
